08dcdcd712d873744061d69cc63519e052bea7cc
John Flynn <johnflynn@users.noreply.github.com>
2016-03-05 16:18:41 +0000

    Move ParameterSlider class to own .h file.

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -13,48 +13,8 @@
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "PluginProcessor.h"
+#include "ParameterSlider.h"
 
-
-//==============================================================================
-// This is a handy slider subclass that controls an AudioProcessorParameter
-// (may move this class into the library itself at some point in the future..)
-class ParameterSlider   : public Slider,
-                          private Timer
-{
-public:
-    ParameterSlider (AudioProcessorParameter& p)
-        : Slider (p.getName (256)), param (p)
-    {
-        setRange (0.0, 1.0, 0.0);
-        startTimerHz (30);
-        updateSliderPos();
-    }
-
-    void valueChanged() override
-    {
-        param.setValueNotifyingHost ((float) Slider::getValue());
-    }
-
-    void timerCallback() override       { updateSliderPos(); }
-
-    void startedDragging() override     { param.beginChangeGesture(); }
-    void stoppedDragging() override     { param.endChangeGesture();   }
-
-    double getValueFromText (const String& text) override   { return param.getValueForText (text); }
-    String getTextFromValue (double value) override         { return param.getText ((float) value, 1024); }
-
-    void updateSliderPos()
-    {
-        const float newValue = param.getValue();
-
-        if (newValue != (float) Slider::getValue() && ! isMouseButtonDown())
-            Slider::setValue (newValue);
-    }
-
-    AudioProcessorParameter& param;
-
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ParameterSlider)
-};
 
 //==============================================================================
 /**
@@ -73,7 +33,7 @@ private:
     enum SymbolicIndexNames     // Use symbolic names instead of
     {                           // managedParameters index numbers.
         boolName,               // Must have the same ordering as
-        floatName,              // processor's managedParameters!
+        floatName,              // processor's managedParameters!!!
         intName
     };
 
